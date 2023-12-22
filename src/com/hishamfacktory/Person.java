98fb1a57afd48609cdccde4e5f59ee7390817f8d
HishamMohammed <hishammohammed1199@gamil.com>
2023-12-22 05:30:56 +0300

    Implemented functions on Person class that needed for subclasses like Dealer and Player.

--- src/com/hishamfacktory/Person.java
+++ src/com/hishamfacktory/Person.java
@@ -1,4 +1,31 @@
 package com.hishamfacktory;
 
 public class Person {
+    private String name;
+    private Hand hand;
+
+    public Person(String name, Hand hand){
+        this.name = name;
+        this.hand = hand;
+    }
+    public Hand getHand(){
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void hit(Deck deck,Deck discard){
+        deck.addCard(discard.takeCard());
+    }
+    public boolean hasBlackJack(){
+        if(hand.calculateValue() == 21) return true;
+        else return false;
+    }
 }
